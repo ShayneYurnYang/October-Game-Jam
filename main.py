6ed0e6bbcc422638aaf7c8782f75485e3cf99f60
Canrries Hong <canrrieshong@gmail.com>
2025-10-12 22:49:43 -0400

    text

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ move_right = False
 move_left = False
 dt = 0
 player_pos = pygame.Vector2(0, 0)
+keys = pygame.key.get_pressed()
 
 #set window name
 pygame.display.set_caption("October game jam")
@@ -22,6 +23,62 @@ pygame.display.set_caption("October game jam")
 #import and scale background image
 bg = pygame.image.load("bg.png").convert()
 bg = pygame.transform.scale(bg,(bg.get_width()*constants.SCREEN_HEIGHT/bg.get_height(),constants.SCREEN_HEIGHT))
+
+
+class Text:
+    def __init__(self):
+        self.msg = []
+        self.printmsg = []
+        self.start = 0
+        self.counter = 0
+        self.row = 0
+        self.length = 0
+        self.lineHeight = 20
+        self.keyPress = True
+        
+    def print(self, msg, time, name = ""):
+        split = msg.split("\n")
+        self.msg.append((split, time, name))
+        self.printmsg=[" "]*len(self.msg[0][0])
+        for i in self.msg[0][0]:
+            self.length += len(i)
+
+            
+    
+    def update(self, keys):
+        if len(self.msg) != 0:
+            self.start += dt
+            pygame.draw.polygon(screen, (255,255,255), [(constants.SCREEN_WIDTH/8, constants.SCREEN_HEIGHT/2),(constants.SCREEN_WIDTH/8*7, constants.SCREEN_HEIGHT/2),(constants.SCREEN_WIDTH/8*7, constants.SCREEN_HEIGHT),(constants.SCREEN_WIDTH/8, constants.SCREEN_HEIGHT)], width=0)
+            pygame.draw.polygon(screen, (0,0,0), [(constants.SCREEN_WIDTH/8, constants.SCREEN_HEIGHT/2),(constants.SCREEN_WIDTH/8*7, constants.SCREEN_HEIGHT/2),(constants.SCREEN_WIDTH/8*7, constants.SCREEN_HEIGHT),(constants.SCREEN_WIDTH/8, constants.SCREEN_HEIGHT)], width=2)
+            
+            for index, msg in enumerate(self.printmsg, start=1):
+                screen.blit(pygame.font.SysFont("arial", 20).render(msg, False, (0, 0, 0)), (constants.SCREEN_WIDTH/8+100, constants.SCREEN_HEIGHT/2+10+self.lineHeight*index))
+
+            screen.blit(pygame.font.SysFont("arial", 20).render(self.msg[0][2], False, (0, 0, 0)), (constants.SCREEN_WIDTH/8+8, constants.SCREEN_HEIGHT/2+2))
+            
+
+            if self.start >= self.msg[0][1]/self.length and self.printmsg != self.msg[0][0]:
+                    self.printmsg[self.row]=(self.msg[0][0][self.row][:self.counter])
+                    if self.counter != len(self.msg[0][0][self.row]):
+                        self.counter += 1
+                    else:
+                        self.row += 1
+                        self.counter = 0
+                    self.start = 0
+                
+            if keys[pygame.K_RETURN] and self.keyPress:
+                self.msg.pop(0)
+                if len(self.msg) != 0:
+                    self.printmsg=[" "]*len(self.msg[0][0])
+                self.counter = 0
+                self.row = 0
+                self.keyPress = False
+                
+            if not keys[pygame.K_RETURN] and self.keyPress == False:
+                self.keyPress = True
+gg = True
+
+text = Text()
 
 #main game loop
 while running:
@@ -32,9 +89,10 @@ while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     screen.fill("black")
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         #exit program
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
         
         #key pressed
@@ -72,14 +130,24 @@ while running:
         screen.blit(bg,(-(bg.get_width()-constants.SCREEN_WIDTH),0))
         pygame.draw.circle(screen, "red",(screen.get_width()/2+player_pos.x-(bg.get_width()-constants.SCREEN_WIDTH),screen.get_height()/2),constants.PLAYER_WIDTH/2)
 
+
+
     #debug
     if debug:
-        print(str(player_pos.x)+", "+str((bg.get_width()-constants.SCREEN_WIDTH/2)))
+        # print(str(player_pos.x)+", "+str((bg.get_width()-constants.SCREEN_WIDTH/2)))
+        # print(dt)
+        if gg:
+            text.print("hello guys made some very minor updates", 2, "person 1")
+            text.print("but it can support multiple lines now", 2, "person 2")
+            text.print("which it could not do before \n \nfor some reason", 2, "person 1")
+            text.print("still ugly though", 2, "person 2")
+            text.print("what else should i change apart from the font", 2)
+            gg=False
+        # print(dt)
     
     # flip() the display to put your work on screen
+    text.update(keys)
     pygame.display.flip()
 
 #clean up program
 pygame.quit()
-
-
